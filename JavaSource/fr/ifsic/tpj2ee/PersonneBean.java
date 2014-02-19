d91a134a21e292bb02f7f559cd21a6c8a2a841d6
13000018 <13000018@e007m05.istic.univ-rennes1.fr>
2014-02-19 18:12:08 +0100

    JSF

---
+++ JavaSource/fr/ifsic/tpj2ee/PersonneBean.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fr.ifsic.tpj2ee;
+
+/**
+ * @author 13000018
+ *
+ */
+public class PersonneBean {
+	private String name;
+
+	public PersonneBean() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ WebContent/debut.jsp
@@ -0,0 +1,5 @@
+<html>
+ <body>
+  <jsp:forward page="test.jsf" />
+ </body>
+</html>
